d80880ff76073da584ee7e5732f25f863b640db4
squach28 <squach3443@gmail.com>
2023-03-20 14:21:14 -0700

    infinite scroll for infinite shibas >:]

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import { Roboto } from 'next/font/google'
 import Link from 'next/link'
 import Image from 'next/image'
 import '../styles/Home.module.css'
+import { useEffect, useRef, useState, useCallback } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 
 const roboto = Roboto({
     weight: '400',
@@ -9,8 +11,28 @@ const roboto = Roboto({
 
   })
 
-export default function Home({shibes}) {
-  console.log(shibes)
+export default function Home({shibas}) {
+
+  const [shibes, setShibes] = useState([...shibas])
+  const [position, setPosition] = useState(0)
+
+  const observer = useRef()
+  const lastShibeElementRef = useCallback(node => {
+    if(observer.current) observer.current.disconnect()
+    observer.current = new IntersectionObserver(entries => {
+      if(entries[0].isIntersecting) {
+        fetch('http://shibe.online/api/shibes?count=9&urls=true&httpsUrls=true')
+        .then(shibas => shibas.json())
+        .then(shibesJSON => setShibes(oldShibes => [...oldShibes, ...shibesJSON]))
+        .finally(() => {
+        })
+      }
+    })
+    if(node) observer.current.observe(node)
+  }, [])
+
+
+
   return (
     <div className="h-screen w-screen p-5">
       <header>
@@ -22,7 +44,14 @@ export default function Home({shibes}) {
         <div>
           <h2 className="text-lg text-center my-20">The website to truly experience the best of shibes</h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-5 justify-items-center">
-            {shibes.map(shibe => <Image key={shibe} width={400} height={400} src={shibe} alt="it's a shibe" />
+            {shibes.map((shibe, index) => {
+              if(shibes.length === index + 1) {
+                return <Image ref={lastShibeElementRef} key={uuidv4()} width={400} height={400} src={shibe} alt="it's a shibe" />
+              } else {
+                return <Image key={uuidv4()} width={400} height={400} src={shibe} alt="it's a shibe" />
+
+              }
+            }
             )}
           </div>
         </div>
@@ -35,7 +64,7 @@ export async function getStaticProps() {
   const shibesJSON = await shibes.json()
   return {
     props: {
-      shibes: shibesJSON
+      shibas: shibesJSON
     }
   }
 }
